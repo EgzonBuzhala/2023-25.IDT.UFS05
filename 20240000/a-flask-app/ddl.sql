4961f6d6222e19c9414ff9b9f19da84bffc8d59a
Egzon <157045626+EgzonBuzhala@users.noreply.github.com>
2024-07-02 09:04:05 +0000

    a

--- 20240000/a-flask-app/ddl.sql
+++ 20240000/a-flask-app/ddl.sql
@@ -1,12 +1,12 @@
-CREATE DATABASE `egzondb` ;
-use `egzondb` ;
+CREATE DATABASE egzondb ;
+use egzondb ;
 CREATE TABLE `employees` (
-  `first_name` varchar(45) DEFAULT NULL,
-  `last_name` varchar(45) DEFAULT NULL
+  first_name varchar(45) DEFAULT NULL,
+  last_name varchar(45) DEFAULT NULL
 );
 
-INSERT INTO `egzondb`.`employees`
-(`first_name`,
-`last_name`)
+INSERT INTO egzondb.employees
+(first_name,
+last_name)
 VALUES
 ('marco', 'booo');
